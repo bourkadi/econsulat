5c3e44b7c9137835f136cfa7cab5beb6d9ad64f1
Bourkadi <Bourkadi@192.168.1.104>
2018-05-09 23:28:25 +0400

    Adding the latest consular.sql version

--- src/main/resources/consular.sql
+++ src/main/resources/consular.sql
@@ -1,324 +1,256 @@
--- phpMyAdmin SQL Dump
--- version 4.5.3.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
 --
--- Host: localhost
--- Generation Time: Apr 17, 2018 at 09:59 PM
--- Server version: 5.7.21-0ubuntu0.17.10.1
--- PHP Version: 7.1.15-0ubuntu0.17.10.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: consular
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `consular`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `attestation`
 --
 
+DROP TABLE IF EXISTS `attestation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `attestation` (
-  `idattestation` int(11) NOT NULL,
+  `idattestation` int(11) NOT NULL AUTO_INCREMENT,
   `createdDate` int(11) NOT NULL,
   `content` text NOT NULL,
   `user` int(11) NOT NULL COMMENT 'creator of the attestation\n',
   `person` int(11) NOT NULL,
-  `attestation_type` int(11) NOT NULL
+  `attestation_type` int(11) NOT NULL,
+  PRIMARY KEY (`idattestation`),
+  KEY `fk_attestation_user1_idx` (`user`),
+  KEY `fk_attestation_person1_idx` (`person`),
+  KEY `fk_attestation_attestation_type1_idx` (`attestation_type`),
+  FULLTEXT KEY `fl_attestaton_content` (`content`),
+  CONSTRAINT `fk_attestation_attestation_type1` FOREIGN KEY (`attestation_type`) REFERENCES `attestation_type` (`idattestation_type`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_attestation_person1` FOREIGN KEY (`person`) REFERENCES `person` (`idperson`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_attestation_user1` FOREIGN KEY (`user`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=dec8 COMMENT='A service given to the citizen';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `attestation`
+--
+
+LOCK TABLES `attestation` WRITE;
+/*!40000 ALTER TABLE `attestation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attestation` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `attestation_type`
 --
 
+DROP TABLE IF EXISTS `attestation_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `attestation_type` (
-  `idattestation_type` int(11) NOT NULL,
-  `type` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `idattestation_type` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`idattestation_type`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `attestation_type`
 --
 
-INSERT INTO `attestation_type` (`idattestation_type`, `type`) VALUES
-(1, 'Attestation de Célibat'),
-(2, 'Act de Naissance'),
-(3, 'Déclaration sur l\'honneur');
-
--- --------------------------------------------------------
+LOCK TABLES `attestation_type` WRITE;
+/*!40000 ALTER TABLE `attestation_type` DISABLE KEYS */;
+INSERT INTO `attestation_type` VALUES (1,'Attestation de Célibat'),(2,'Act de Naissance'),(3,'Déclaration sur l\'honneur');
+/*!40000 ALTER TABLE `attestation_type` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `consulate`
 --
 
+DROP TABLE IF EXISTS `consulate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `consulate` (
-  `idconsulat` int(11) NOT NULL,
+  `idconsulat` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `longName` varchar(45) DEFAULT NULL,
-  `country` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `country` int(11) NOT NULL,
+  PRIMARY KEY (`idconsulat`),
+  KEY `fk_consulat_country1_idx` (`country`),
+  CONSTRAINT `fk_consulat_country1` FOREIGN KEY (`country`) REFERENCES `country` (`idcountry`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `consulate`
 --
 
-INSERT INTO `consulate` (`idconsulat`, `name`, `longName`, `country`) VALUES
-(3, 'DUBAI', 'LE CONSULAT GENERAL DU MAROC A DUBAI', 1),
-(4, 'ABU DHABI', 'AMBASSADE DU ROYUAME A ABU DHABI', 1);
-
--- --------------------------------------------------------
+LOCK TABLES `consulate` WRITE;
+/*!40000 ALTER TABLE `consulate` DISABLE KEYS */;
+INSERT INTO `consulate` VALUES (3,'DUBAI','LE CONSULAT GENERAL DU MAROC A DUBAI',1),(4,'ABU DHABI','AMBASSADE DU ROYUAME A ABU DHABI',1);
+/*!40000 ALTER TABLE `consulate` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `country`
 --
 
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `country` (
-  `idcountry` int(11) NOT NULL,
+  `idcountry` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `longName` varchar(45) DEFAULT NULL,
-  `continent` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='A country table, contains many consulates, ';
+  `continent` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idcountry`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='A country table, contains many consulates, ';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `country`
 --
 
-INSERT INTO `country` (`idcountry`, `name`, `longName`, `continent`) VALUES
-(1, 'UAE', 'United Arab Emirates', 'ASIA'),
-(2, 'France', 'La République Française', 'Europe');
-
--- --------------------------------------------------------
+LOCK TABLES `country` WRITE;
+/*!40000 ALTER TABLE `country` DISABLE KEYS */;
+INSERT INTO `country` VALUES (1,'UAE','United Arab Emirates','ASIA'),(2,'France','La République Française','Europe');
+/*!40000 ALTER TABLE `country` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `person`
 --
 
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `person` (
-  `idperson` int(11) NOT NULL,
+  `idperson` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `lastName` varchar(45) NOT NULL,
   `birthdate` date DEFAULT NULL,
   `cnie` varchar(45) NOT NULL COMMENT 'CNIE or ETAT CIVIL',
   `ic` varchar(45) NOT NULL COMMENT 'IC, le numéro consulaire, imported from the CONSULAIRE app or just created',
-  `consulate` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='A citizen, mostly imported from the CONSULAIRE app';
+  `consulate` int(11) NOT NULL,
+  PRIMARY KEY (`idperson`),
+  KEY `fk_person_consulate1_idx` (`consulate`),
+  FULLTEXT KEY `fl_person_name` (`name`,`lastName`),
+  FULLTEXT KEY `fl_person_cnie` (`cnie`),
+  FULLTEXT KEY `fl_person_ic` (`ic`),
+  CONSTRAINT `fk_person_consulate1` FOREIGN KEY (`consulate`) REFERENCES `consulate` (`idconsulat`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='A citizen, mostly imported from the CONSULAIRE app';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `person`
 --
 
-INSERT INTO `person` (`idperson`, `name`, `lastName`, `birthdate`, `cnie`, `ic`, `consulate`) VALUES
-(1, 'amine', 'bourkadi', '2017-03-07', 'jddnen', 'bfdbde', 3);
-
--- --------------------------------------------------------
+LOCK TABLES `person` WRITE;
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+INSERT INTO `person` VALUES (1,'amine','bourkadi','2017-03-07','jddnen','bfdbde',3);
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `role`
 --
 
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
-  `idrole` int(11) NOT NULL,
-  `role` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='roles of the users, defines ACL';
+  `idrole` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idrole`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='roles of the users, defines ACL';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `role`
 --
 
-INSERT INTO `role` (`idrole`, `role`) VALUES
-(1, 'ADMIN'),
-(2, 'SAISI');
-
--- --------------------------------------------------------
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'ADMIN'),(2,'SAISI');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `iduser` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `password` varchar(128) NOT NULL,
   `createdDate` int(11) NOT NULL,
-  `consulate` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='user of the system, attached to an entity, mostly a consulat';
+  `consulate` int(11) NOT NULL,
+  `connected` tinyint(1) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT NULL,
+  `last_connected_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`iduser`),
+  KEY `fk_user_consulat1_idx` (`consulate`),
+  CONSTRAINT `fk_user_consulat1` FOREIGN KEY (`consulate`) REFERENCES `consulate` (`idconsulat`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='user of the system, attached to an entity, mostly a consulat';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`iduser`, `username`, `password`, `createdDate`, `consulate`) VALUES
-(1, 'amine', 'amine', 12345, 3);
-
--- --------------------------------------------------------
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (2,'amine','cd06978490e672682031fd13de989a5311c078718571b2d3f9c1aa039161da3feac29ead85d7b1609b367a1181f985e75f0ba95574c9d666f480bd3444826623',12345,3,1,1,11111),(3,'moh','12345667889',12345678,3,1,1,132467889);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_has_role`
 --
 
+DROP TABLE IF EXISTS `user_has_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_has_role` (
   `iduser` int(11) NOT NULL,
-  `role_idrole` int(11) NOT NULL
+  `role_idrole` int(11) NOT NULL,
+  PRIMARY KEY (`iduser`,`role_idrole`),
+  KEY `fk_user_has_role_role1_idx` (`role_idrole`),
+  KEY `fk_user_has_role_user_idx` (`iduser`),
+  CONSTRAINT `fk_user_has_role_role1` FOREIGN KEY (`role_idrole`) REFERENCES `role` (`idrole`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_role_user` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user_has_role`
 --
 
-INSERT INTO `user_has_role` (`iduser`, `role_idrole`) VALUES
-(1, 1),
-(1, 2);
+LOCK TABLES `user_has_role` WRITE;
+/*!40000 ALTER TABLE `user_has_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_has_role` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `attestation`
---
-ALTER TABLE `attestation`
-  ADD PRIMARY KEY (`idattestation`),
-  ADD KEY `fk_attestation_user1_idx` (`user`),
-  ADD KEY `fk_attestation_person1_idx` (`person`),
-  ADD KEY `fk_attestation_attestation_type1_idx` (`attestation_type`);
-ALTER TABLE `attestation` ADD FULLTEXT KEY `fl_attestaton_content` (`content`);
-
---
--- Indexes for table `attestation_type`
---
-ALTER TABLE `attestation_type`
-  ADD PRIMARY KEY (`idattestation_type`);
-
---
--- Indexes for table `consulate`
---
-ALTER TABLE `consulate`
-  ADD PRIMARY KEY (`idconsulat`),
-  ADD KEY `fk_consulat_country1_idx` (`country`);
-
---
--- Indexes for table `country`
---
-ALTER TABLE `country`
-  ADD PRIMARY KEY (`idcountry`);
-
---
--- Indexes for table `person`
---
-ALTER TABLE `person`
-  ADD PRIMARY KEY (`idperson`),
-  ADD KEY `fk_person_consulate1_idx` (`consulate`);
-ALTER TABLE `person` ADD FULLTEXT KEY `fl_person_name` (`name`,`lastName`);
-ALTER TABLE `person` ADD FULLTEXT KEY `fl_person_cnie` (`cnie`);
-ALTER TABLE `person` ADD FULLTEXT KEY `fl_person_ic` (`ic`);
-
---
--- Indexes for table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`idrole`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`iduser`),
-  ADD KEY `fk_user_consulat1_idx` (`consulate`);
-
---
--- Indexes for table `user_has_role`
---
-ALTER TABLE `user_has_role`
-  ADD PRIMARY KEY (`iduser`,`role_idrole`),
-  ADD KEY `fk_user_has_role_role1_idx` (`role_idrole`),
-  ADD KEY `fk_user_has_role_user_idx` (`iduser`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `attestation`
---
-ALTER TABLE `attestation`
-  MODIFY `idattestation` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `attestation_type`
---
-ALTER TABLE `attestation_type`
-  MODIFY `idattestation_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `consulate`
---
-ALTER TABLE `consulate`
-  MODIFY `idconsulat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `country`
---
-ALTER TABLE `country`
-  MODIFY `idcountry` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `person`
---
-ALTER TABLE `person`
-  MODIFY `idperson` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-  MODIFY `idrole` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `attestation`
---
-ALTER TABLE `attestation`
-  ADD CONSTRAINT `fk_attestation_attestation_type1` FOREIGN KEY (`attestation_type`) REFERENCES `attestation_type` (`idattestation_type`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_attestation_person1` FOREIGN KEY (`person`) REFERENCES `person` (`idperson`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_attestation_user1` FOREIGN KEY (`user`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `consulate`
---
-ALTER TABLE `consulate`
-  ADD CONSTRAINT `fk_consulat_country1` FOREIGN KEY (`country`) REFERENCES `country` (`idcountry`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `person`
---
-ALTER TABLE `person`
-  ADD CONSTRAINT `fk_person_consulate1` FOREIGN KEY (`consulate`) REFERENCES `consulate` (`idconsulat`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `fk_user_consulat1` FOREIGN KEY (`consulate`) REFERENCES `consulate` (`idconsulat`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `user_has_role`
---
-ALTER TABLE `user_has_role`
-  ADD CONSTRAINT `fk_user_has_role_role1` FOREIGN KEY (`role_idrole`) REFERENCES `role` (`idrole`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_user_has_role_user` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-03 21:49:06
